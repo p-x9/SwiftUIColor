9505db3f879950f07212987e6f2891612e968779
p-x9 <50244599+p-x9@users.noreply.github.com>
2023-01-03 00:13:56 +0900

    fix to be able to specify name

--- scripts/create_artifactbundle.sh
+++ scripts/create_artifactbundle.sh
@@ -1,13 +1,26 @@
 #!/usr/bin/env bash
 
+# example
+#
+# - version
+# scripts/create_artifactbundle.sh 1.0.0
+#
+# - version with artifactbundle name
+# scripts/create_artifactbundle.sh 1.0.0 prepare-swiftui-color-bin
+
 set -Ceu
 
 VERSION_STRING=$1
 
 PRODUCT_NAME="prepare-swiftui-color"
+NAME=$PRODUCT_NAME
 
-ARTIFACT_BUNDLE_NAME="${PRODUCT_NAME}.artifactbundle"
-ARTIFACT_BUNDLE_BIN_PATH="${ARTIFACT_BUNDLE_NAME}/${PRODUCT_NAME}/bin"
+if (( $# > 1 )); then
+    NAME=$2
+fi
+
+ARTIFACT_BUNDLE_NAME="${NAME}.artifactbundle"
+ARTIFACT_BUNDLE_BIN_PATH="${ARTIFACT_BUNDLE_NAME}/${NAME}/bin"
 
 INFO_TEMPLATE="./scripts/info.json.template"
 
@@ -20,10 +33,12 @@ show_checksum() {
 create_artifact_bundle() {
     mkdir -p "${ARTIFACT_BUNDLE_BIN_PATH}"
 
-    sed "s/__VERSION__/${VERSION_STRING}/g" ${INFO_TEMPLATE} > "${ARTIFACT_BUNDLE_NAME}/info.json"
+    sed "s/__VERSION__/${VERSION_STRING}/g; s/__NAME__/${NAME}/g" ${INFO_TEMPLATE} > "${ARTIFACT_BUNDLE_NAME}/info.json"
 
     cp -f ".build/apple/Products/Release/${PRODUCT_NAME}" "${ARTIFACT_BUNDLE_BIN_PATH}"
     cp -f LICENSE "${ARTIFACT_BUNDLE_NAME}"
+
+    mv "${ARTIFACT_BUNDLE_BIN_PATH}/${PRODUCT_NAME}" "${ARTIFACT_BUNDLE_BIN_PATH}/${NAME}"
 }
 
 zip_artifact_bundle() {
@@ -43,7 +58,7 @@ clear_outputs() {
 clear_outputs
 
 if [ "$1" = "--clear" ]; then
- exit
+    exit
 fi
 
 build_target
