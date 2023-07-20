9b8acf4f6ae913fbb81b6ef8a184573eb8788073
p-x9 <50244599+p-x9@users.noreply.github.com>
2023-07-20 15:24:49 +0900

    remove plugin-related files

--- Package.swift
+++ Package.swift
@@ -19,36 +19,18 @@ let package = Package(
             targets: ["prepare-swiftui-color"]
         )
     ],
-    dependencies: [
-    ],
+    dependencies: [],
     targets: [
         .target(
             name: "SwiftUIColor",
             dependencies: [],
             resources: [
                 .process("Assets")
-            ],
-            plugins: [
-                .plugin(name: "PrepareSwiftUIColor")
-            ]
-        ),
-        .plugin(
-            name: "PrepareSwiftUIColor",
-            capability: .buildTool(),
-            dependencies: [
-                "prepare-swiftui-color-bin"
             ]
         ),
         .executableTarget(
             name: "prepare-swiftui-color"
         ),
-        .binaryTarget(
-            name: "prepare-swiftui-color-bin",
-            url: "https://github.com/p-x9/SwiftUIColor/releases/download/0.0.6/prepare-swiftui-color-bin.artifactbundle.zip",
-            checksum: "d265000d699ed331749eaf2e1763d4f4ccd0b697f4e39a3df6ce5843c65f0a48"
-        ),
-//        DUBUG
-//        .binaryTarget(name: "prepare-swiftui-color-bin", path: "./prepare-swiftui-color-bin.artifactbundle.zip"),
         .testTarget(
             name: "SwiftUIColorTests",
             dependencies: ["SwiftUIColor"]
